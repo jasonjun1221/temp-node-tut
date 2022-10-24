aaa0d5b89817751b383c2cc53ca8f9fcfaf99862
Jason <jasonchong1221@hotmail.com>
2022-10-24 12:31:41 +0000

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//const names = require('./4-names.js')
+const {john, peter} = require('./4-names.js')
+const sayHi = require('./5-utils.js')
+const data = require('./6-alternative-flavor.js')
+require('./7-mind-grenade.js')
+
+sayHi('Susan')
+//sayHi(names.john)
+//sayHi(names.peter)
+//console.log(data)
+sayHi(john)
+sayHi(peter)
